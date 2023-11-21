f781f3f68c1fb10b14e384afdd754b58e0809c1e
红血球AE3803 <2544390577@qq.com>
2023-11-21 23:03:30 +0900

    Update util.go

--- drivers/url_tree/util.go
+++ drivers/url_tree/util.go
@@ -186,7 +186,11 @@ func getSizeFromUrl(url string) (int64, error) {
 	}
 	size, err := strconv.ParseInt(res.Header().Get("Content-Length"), 10, 64)
 	if err != nil {
-		return 0, err
+		size, err := strconv.ParseInt(res.Header().Get("Accept-Length"), 9, 64)
+		if err != nil {
+			return 0, err
+		}
+		return size, nil
 	}
 	return size, nil
 }
